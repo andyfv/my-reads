b4db236cfcafc0882073533c3bd17a863a5b2e25
Andy <andyfilchev@gmail.com>
2018-07-30 11:56:01 +0300

    feat: Add react-bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Panel, Collapse } from 'react-bootstrap'
 class App extends Component {
   render() {
     return (
