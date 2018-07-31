98cd24a318c597534dec28cedd88481d2df877c4
Andy <andyfilchev@gmail.com>
2018-08-01 01:27:03 +0300

    refactor: Now using react-route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Panel, Collapse } from 'react-bootstrap'
+import { Route } from 'react-router-dom'
+import SearchPage from './SearchPage'
+import FrontPage from './FrontPage'
+import 'normalize.css'
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/" render={() => (
+          <FrontPage searchPage={SearchPage}/>
+        )}/>
+        <Route exact path="/search" render={({history}) => (
+          <SearchPage />
+        )}/>
       </div>
     );
   }
