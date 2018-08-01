34b68ddbe545e596dde6af009c492a8f3cf31294
Andy <andyfilchev@gmail.com>
2018-08-01 11:26:36 +0300

    feat: Updated to use the Page Component

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
-import { Panel, Collapse } from 'react-bootstrap'
-
+import Shelf from './Shelf'
+import Page from './Page'
+import FrontPageHeader from './FrontPageHeader'
 class FrontPage extends Component {
-
-
     render() {
         return (
-            <div className="page">
-                <header className="home-header">
-                    <div className="home-header-wrapper">
-                        <ol className="header-items">
-                            <li id="left-area">
-
-                            </li>
-                            <li id="central-area">
-                                <p id="title">MyReads</p>
-                            </li>
-                            <li id="right-area">
-                                <button id="search-btn">
-                                    <svg id="search-icon"></svg>
-                                </button>
-                            </li>
-                        </ol>
-                    </div>
-                </header>
-                <div className="home-content">
-
-                </div>
-            </div>
+            <Page
+                headerContent={<FrontPageHeader />}
+                bodyContent={[
+                    <Shelf title={'Reading'}/>,
+                    <Shelf title={'Want to Read'}/>,
+                    <Shelf title={'Want to Read'}/>]}
+            />
         )
     }
 }
