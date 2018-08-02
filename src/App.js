3b2bee8d828ae533016dc3abdb679b30b817612f
Andy <andyfilchev@gmail.com>
2018-08-02 20:06:41 +0300

    feat: Move BooksAPI to src/util folder and use it src/App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import * as BooksAPI from './utils/BooksAPI'
+import PropTypes from 'prop-types'
 import SearchPage from './SearchPage'
-import FrontPage from './FrontPage'
+import HomePage from './HomePage'
 import 'normalize.css'
+
 class App extends Component {
+
+  state = { books: [] }
+  
+  updateBookShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(this.componentDidMount())
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books: books})
+    })
+  }         
 
   render() {
     return (
       <div className="App">
         <Route exact path="/" render={() => (
-          <FrontPage searchPage={SearchPage}/>
-        )}/>
+          <HomePage
+            books={this.state.books}
+            updateBookShelf={this.updateBookShelf}
+          />
+        )}
+        />
         <Route exact path="/search" render={({history}) => (
-          <SearchPage />
+          <SearchPage 
+            books={this.state.books}
+            updateBookShelf={this.updateBookShelf}
+          />
         )}/>
       </div>
     );
@@ -20,3 +42,16 @@ class App extends Component {
 }
 
 export default App;
+
+
+{
+  /* <Route exact path="/" render={() => (
+            <FrontPage searchPage={SearchPage}/>
+          )}/> */
+}
+
+{
+  /* <Route exact path="/search" render={({history}) => (
+            <SearchPage />
+          )}/> */
+}
