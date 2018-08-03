e9b5982f29cf87d1539d0c08e1263a2e5363a6f2
Andy <andyfilchev@gmail.com>
2018-08-03 11:20:42 +0300

    feat: Add athors and thunmbnail verification

--- src/Book.js
+++ src/Book.js
@@ -4,19 +4,22 @@ import ProptType from 'prop-types'
 class Book extends Component {
 
     render() {
+        let authors = this.props.book.authors ?
+                        this.props.book.authors : [''];
+        let thumbnail = this.props.book.imageLinks ?
+                        this.props.book.imageLinks.thumbnail : ''
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+ this.props.book.imageLinks.thumbnail +')' }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + thumbnail + ')' }}></div>
                         <div className="book-shelf-changer">
                             <select
-                                onChange={(event) => this.props.updateBookShelf(this.props.book,event.target.value)}
+                                onChange={(event) => {
+                                    this.props.updateBookShelf(this.props.book,event.target.value)
+                                }}
                                 value={this.props.book.shelf}
                             >
-                                <optgroup label="Move To">
-                                    <option value="here">Here</option>
-                                </optgroup>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
@@ -24,10 +27,9 @@ class Book extends Component {
                                 <option value="none">None</option>
                             </select>
                         </div>
-                        <div className="book-shelf-selected"></div>
                     </div>
                     <div className="book-title">{this.props.book.title}</div>
-                    <div className="book-authors">{this.props.book.authors.map((author) => author)}</div>
+                    <div className="book-authors">{authors[0]}</div>
                 </div>
             </li>
         )
